semgrep/bento

commit 662c2e23b3562450219fd6173fa2b08ad985c51b

Clean up telemetry (#252)
* Bento now sends teleemtry data to v4 routes. (#251)

It fixes the problem of data from older and newer versions of Bento getting mixed up in our telemetry database.

Bento now sends its data to v4 routes, which send it to Segment with new event names

* Telemetry nits

* add back  and not break kibana dash

bento/metrics.py
@@ -32,7 +32,6 @@ def __get_aggregate_violations(violations: List[Violation]) -> List[Dict[str, An
         out.append(
             {
                 "path_hash": __hash_sha256(p),
-                "check_id_hash": __hash_sha256(rid),
                 "check_id": rid,
                 "count": sum(1 for _ in v),
                 "filtered_count": __get_filtered_violation_count(v),
@@ -44,14 +43,13 @@ def __get_aggregate_violations(violations: List[Violation]) -> List[Dict[str, An
 def violations_to_metrics(
     tool_id: str, timestamp: str, violations: List[Violation], ignores: List[str]
 ) -> List[Dict[str, Any]]:
-    git_url = __hash_sha256(bento.git.url())
-    git_commit = bento.git.commit()
     return [
         {
             "tool": tool_id,
             "timestamp": timestamp,
-            "repository": git_url,
-            "commit": git_commit,
+            "hash_of_repository": __hash_sha256(bento.git.url()),
+            "repository": __hash_sha256(bento.git.url()),
+            "hash_of_commit": __hash_sha256(bento.git.commit()),
             "ignored_rules": ignores,
             **aggregates,
         }
@@ -76,12 +74,15 @@ def command_metric(
     duration: float,
     exception: Optional[Exception],
 ) -> List[Dict[str, Any]]:
+    email = read_user_email()
     d = {
         "timestamp": timestamp,
         "duration": duration,
         "exit_code": exit_code,
+        "hash_of_repository": __hash_sha256(bento.git.url()),
         "repository": __hash_sha256(bento.git.url()),
-        "email": read_user_email(),
+        "email": email,
+        "user": __hash_sha256(email),
         "hash_of_commit": __hash_sha256(bento.git.commit()),
         "command": command,
         "command_kwargs": command_kwargs,

bento/network.py
@@ -80,7 +80,7 @@ def fetch_latest_version() -> Tuple[Optional[str], Optional[str]]:
 
 def post_metrics(data: PostData, is_finding: bool = False) -> bool:
     try:
-        url = f"{_get_base_url()}/bento/api/v1/metrics/"
+        url = f"{_get_base_url()}/bento/api/v4/metrics/"
         if is_finding:
             url = f"{url}finding/"
         r = no_auth_post(url, json=data)

commit 02da854fec1eb969b5d263963bdf8c9af7c154be

metrics: Fix performance regression (#295)
662c2e23 introduced a performance regression, introducing again the file
operation issue solved in 051e78eb.

Here I add a strict comment noting this issue, in an effort to prevent
it in the future.

bento/metrics.py
@@ -43,13 +43,17 @@ def __get_aggregate_violations(violations: List[Violation]) -> List[Dict[str, An
 def violations_to_metrics(
     tool_id: str, timestamp: str, violations: List[Violation], ignores: List[str]
 ) -> List[Dict[str, Any]]:
+    # NOTE: Do not calculate url() and commit() on a per-item basis.
+    # Doing so causes bento check to take many unnecessary seconds.
+    url = bento.git.url()
+    commit = bento.git.commit()
     return [
         {
             "tool": tool_id,
             "timestamp": timestamp,
-            "hash_of_repository": __hash_sha256(bento.git.url()),
-            "repository": __hash_sha256(bento.git.url()),
-            "hash_of_commit": __hash_sha256(bento.git.commit()),
+            "hash_of_repository": __hash_sha256(url),
+            "repository": __hash_sha256(url),
+            "hash_of_commit": __hash_sha256(commit),
             "ignored_rules": ignores,
             **aggregates,
         }