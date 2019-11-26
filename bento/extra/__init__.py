8a0f99141c9d3ac6792eee113008318210d151ab
Nathaniel Brahms <nbrahms@users.noreply.github.com>
2019-11-25 16:27:02 -0800

    requests: Add (#362)
    This commit adds checks from flake8-requests as a new r2c.requests
    tool.
    
    An integration test path is added, with accompanying test case.
    
    Also update flake8-requests to 0.2.0 to get Python 3.6 compatibility :)

--- bento/extra/__init__.py
+++ bento/extra/__init__.py
@@ -8,6 +8,7 @@ from bento.extra.flask import FlaskTool
 from bento.extra.grep import GrepTool
 from bento.extra.hadolint import HadolintTool
 from bento.extra.pyre import PyreTool
+from bento.extra.requests import RequestsTool
 from bento.extra.sgrep import SGrepTool
 from bento.extra.shellcheck import ShellcheckTool
 from bento.tool import Tool
@@ -18,9 +19,10 @@ TOOLS: Iterable[Type[Tool]] = [
     EslintTool,
     FlaskTool,
     Flake8Tool,
-    PyreTool,
     GrepTool,
     HadolintTool,
+    PyreTool,
+    RequestsTool,
     SGrepTool,
     ShellcheckTool,
 ]
