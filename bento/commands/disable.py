89d55eabd9bd540973797ba2b33dacfcc19f625c
Brendon Go <brendon.go@gmail.com>
2019-11-26 11:27:31 -0800

    enable/disable: fix help texts (#363)
    Uses better help text copy for enable disable commands

--- bento/commands/disable.py
+++ bento/commands/disable.py
@@ -10,28 +10,45 @@ from bento.util import echo_success
 @click.group()
 def disable() -> None:
     """
-    Disable a tool or check
+        Turn OFF a tool or check.
+
+        Takes a command specifying the tool or check to be disabled.
+
+        For example, to disable the check `no-unused-var` from `r2c.eslint`:
+
+            $ bento disable check r2c.eslint no-unused-var
+
+        To disable the tool `r2c.bandit`:
+
+            $ bento disable tool r2c.bandit
+
+        These commands modify `.bento.yml` in the current project. Tool specific
+        configurations aren't deleted and can be reenabled via `bento enable tool [TOOL]`.
     """
 
 
-@disable.command()
+@disable.command(short_help="Specify tool")
 @click.argument("tool", type=str, nargs=1, autocompletion=get_valid_tools)
 @click.pass_obj
 def tool(context: Context, tool: str) -> None:
     """
-    Disable a tool
+        Turn OFF a tool.
+
+        Please see `bento disable --help` for more information.
     """
     update_tool_run(context, tool, False)
     echo_success(f"{tool} disabled")
 
 
-@disable.command()
+@disable.command(short_help="Specify check")
 @click.argument("tool", type=str, nargs=1, autocompletion=get_valid_tools)
 @click.argument("check", type=str, nargs=1)
 @click.pass_obj
 def check(context: Context, tool: str, check: str) -> None:
     """
-    Disables a check
+        Turn OFF a check.
+
+        Please see `bento disable --help` for more information.
     """
 
     def add(ignores: Set[str]) -> None:
--- bento/commands/enable.py
+++ bento/commands/enable.py
@@ -15,28 +15,47 @@ from bento.util import echo_success
 @click.group()
 def enable() -> None:
     """
-    Here is a help text
+        Turn ON a tool or check.
+
+        Takes a command specifying the tool or check to be enabled.
+
+        For example, to enable the check `no-unused-var` from `r2c.eslint`:
+
+            $ bento enable check r2c.eslint no-unused-var
+
+        To enable the tool `r2c.bandit`:
+
+            $ bento enable tool r2c.bandit
+
+        These commands modify `.bento.yml` in the current project. If the tool was
+        previously enabled and exists in the `.bento.yml`, the tool's previous
+        settings will be used. If no configuration exists, smart defaults will
+        be applied.
     """
 
 
-@enable.command()
+@enable.command(short_help="Specify tool")
 @click.argument("tool", type=str, nargs=1, autocompletion=get_valid_tools)
 @click.pass_obj
 def tool(context: Context, tool: str) -> None:
     """
-        Enable a tool
+        Turn ON a tool.
+
+        See `bento enable --help` for more detail.
     """
     update_tool_run(context, tool, True)
     echo_success(f"{tool} enabled")
 
 
-@enable.command()
+@enable.command(short_help="Specify check")
 @click.argument("tool", type=str, nargs=1, autocompletion=get_valid_tools)
 @click.argument("check", type=str, nargs=1, autocompletion=get_disabled_checks)
 @click.pass_obj
 def check(context: Context, tool: str, check: str) -> None:
     """
-    Enables a check
+        Turn ON a check.
+
+        See `bento enable --help` for more details.
     """
 
     def remove(ignores: Set[str]) -> None:
