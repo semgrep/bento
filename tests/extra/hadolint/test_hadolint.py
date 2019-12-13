2c4f4577fca575f93f8a5d1f922ba23c9bfa7309
Nathaniel Brahms <nbrahms@users.noreply.github.com>
2019-12-13 14:45:30 -0800

    tests: Add test for hadolint (#473)

---
+++ tests/extra/hadolint/test_hadolint.py
@@ -0,0 +1,57 @@
+import os
+from pathlib import Path
+
+from _pytest.tmpdir import tmp_path_factory
+from bento.extra.hadolint import HadolintTool
+from bento.violation import Violation
+from tests.test_tool import context_for
+
+THIS_PATH = Path(os.path.dirname(__file__))
+BASE_PATH = THIS_PATH / ".." / ".." / ".."
+
+
+def test_run(tmp_path_factory: tmp_path_factory) -> None:
+    base_path = BASE_PATH / "tests" / "integration" / "docker"
+    tool = HadolintTool(
+        context_for(tmp_path_factory, HadolintTool.tool_id(), base_path)
+    )
+    tool.setup()
+    violations = tool.results()
+    assert violations == [
+        Violation(
+            tool_id="r2c.hadolint",
+            check_id="DL3006",
+            path="foo.Dockerfile",
+            line=1,
+            column=1,
+            message="Always tag the version of an image explicitly",
+            severity=1,
+            syntactic_context="FROM bar\n",
+            filtered=None,
+            link="https://github.com/hadolint/hadolint/wiki/DL3006",
+        ),
+        Violation(
+            tool_id="r2c.hadolint",
+            check_id="DL3002",
+            path="foo.Dockerfile",
+            line=3,
+            column=1,
+            message="Last USER should not be root",
+            severity=1,
+            syntactic_context="USER root\n",
+            filtered=None,
+            link="https://github.com/hadolint/hadolint/wiki/DL3002",
+        ),
+        Violation(
+            tool_id="r2c.hadolint",
+            check_id="SC2035",
+            path="foo.Dockerfile",
+            line=5,
+            column=1,
+            message="Use ./*glob* or -- *glob* so names with dashes won't become options.",
+            severity=0,
+            syntactic_context="RUN cp * /tmp/foo\n",
+            filtered=None,
+            link="https://github.com/koalaman/shellcheck/wiki/SC2035",
+        ),
+    ]
