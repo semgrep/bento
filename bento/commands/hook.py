bc6e55dfb731166cec1df29572f3827d324cd0b6
Brendon Go <brendon.go@gmail.com>
2019-12-17 13:46:55 -0800

    uninstall_hook: Add command to uninstall bento as a commit hook (#474)
    bento uninstall-hook removes bento pre-commit hook and returns any
    legacy pre-commit hook

--- bento/commands/hook.py
+++ bento/commands/hook.py
@@ -13,6 +13,14 @@ from bento.decorators import with_metrics
 from bento.util import echo_error, echo_success
 
 
+def is_bento_precommit(filename: str) -> bool:
+    if not os.path.exists(filename):
+        return False
+    with open(filename) as f:
+        lines = f.read()
+    return constants.BENTO_TEMPLATE_HASH in lines
+
+
 @click.command()
 @click.pass_obj
 @with_metrics
@@ -24,13 +32,6 @@ def install_hook(context: Context) -> None:
     runs said hook after bento hook is run.
     """
     import git  # import inside def for performance
-
-    def is_bento_precommit(filename: str) -> bool:
-        if not os.path.exists(filename):
-            return False
-        with open(filename) as f:
-            lines = f.read()
-        return constants.BENTO_TEMPLATE_HASH in lines
 
     # Get hook path
     repo = bento.git.repo(context.base_path)
@@ -65,3 +66,37 @@ def install_hook(context: Context) -> None:
         os.chmod(hook_path, original_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
         echo_success(f"Added Bento to your git pre-commit hooks.")
+
+
+@click.command()
+@click.pass_obj
+@with_metrics
+def uninstall_hook(context: Context) -> None:
+    """
+    Uninstalls bento as a git pre-commit hook.
+
+    Returns any pre-bento pre-commit hooks that were placed in .git/hooks/pre-commit.pre-bento
+    into .git/hooks/pre-commit
+    """
+    import git  # import inside def for performance
+
+    # Get hook path
+    repo = bento.git.repo(context.base_path)
+    if repo is None:
+        echo_error("Not a git project")
+        sys.exit(3)
+
+    hook_path = git.index.fun.hook_path("pre-commit", repo.git_dir)
+
+    if not is_bento_precommit(hook_path):
+        echo_error(f"Install hook in {hook_path} is not a bento pre-commit hook")
+        sys.exit(3)
+    else:
+        # Put back legacy hook if exits
+        legacy_hook_path = f"{hook_path}.pre-bento"
+        if os.path.exists(legacy_hook_path):
+            shutil.move(legacy_hook_path, hook_path)
+        else:
+            os.remove(hook_path)
+
+        echo_success(f"Removed Bento from your git pre-commit hooks.")
