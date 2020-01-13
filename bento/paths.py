263c4e58647717856734076885e00146bbaa5df6
Nathaniel Brahms <nbrahms@users.noreply.github.com>
2020-01-13 09:56:56 -0800

    Don't break on merges (#538)
    Currently, two bugs exist when running `bento check` on merge commits:
    1. unmerged files cause bento to crash
    2. merge state is lost (e.g. commit message, diffs from merge)
    
    To fix these issues:
    1. Print a friendly error message when unmerged files exist (we'll still
       require the user to resolve merges before running bento check)
    2. Use checkout instead of reset, in order to preserve merge state
    
    In addition, bento did not properly work with added, deleted, and
    renamed files. I also update Bento to add or remove files as necessary.
    
    Unfortunately, testing was completely manual. Adding test coverage here
    should be a high priority in our next tech debt sprint.

--- bento/paths.py
+++ bento/paths.py
@@ -1,8 +1,12 @@
+import logging
 import os
+import sys
+from collections import namedtuple
 from contextlib import contextmanager
 from pathlib import Path
 from typing import Any, Iterator, List, Optional
 
+import click
 from pre_commit.git import zsplit
 from pre_commit.staged_files_only import staged_files_only
 from pre_commit.util import cmd_output, noop_context
@@ -10,9 +14,18 @@ from pre_commit.util import cmd_output, noop_context
 import bento.git
 from bento.context import Context
 from bento.tool_runner import Comparison, Runner, RunStep
-from bento.util import AutocompleteSuggestions
+from bento.util import AutocompleteSuggestions, echo_error
 
 PATCH_CACHE = str(Path.home() / ".cache" / "bento" / "patches")
+
+
+class StatusCode:
+    Added = "A"
+    Deleted = "D"
+    Renamed = "R"
+    Unmerged = "U"
+    Untracked = "?"
+    Ignored = "!"
 
 
 PathArgument = Optional[List[Path]]
@@ -40,21 +53,91 @@ def _diffed_paths(context: Context, staged_only: bool) -> List[Path]:
     return [Path(p) for p in str_paths]
 
 
+GitStatus = namedtuple("GitStatus", ["added", "removed", "unmerged"])
+
+
+def git_status() -> GitStatus:
+    """
+    Gets added, removed, and unmerged paths from the git index
+    """
+    status_output = (
+        cmd_output("git", "status", "--porcelain", "-z")[1].strip().split("\0")
+    )
+    next_is_file = False
+    added = []
+    removed = []
+    unmerged = []
+    for s in status_output:
+        if not s.strip():
+            continue
+        if next_is_file:
+            # We are in source line of rename
+            next_is_file = False
+            removed.append(s)
+            continue
+        if s[0] == StatusCode.Untracked or s[0] == StatusCode.Ignored:
+            continue
+
+        fname = s[3:]
+        # The following detection for unmerged codes comes from `man git-status`
+        if (
+            s[0] == StatusCode.Added
+            and s[1] == StatusCode.Added
+            or s[0] == StatusCode.Deleted
+            and s[1] == StatusCode.Deleted
+            or s[0] == StatusCode.Unmerged
+            or s[1] == StatusCode.Unmerged
+        ):
+            unmerged.append(fname)
+        if s[0] == StatusCode.Renamed:
+            added.append(fname)
+            next_is_file = True
+        if s[0] == StatusCode.Added:
+            added.append(fname)
+        if s[0] == StatusCode.Deleted:
+            removed.append(fname)
+    logging.info(
+        f"Git status:\nadded: {added}\nremoved: {removed}\nunmerged: {unmerged}"
+    )
+    return GitStatus(added, removed, unmerged)
+
+
 @contextmanager
 def head_context() -> Iterator[None]:
+    """
+    Runs a block of code on files from the current branch HEAD.
+
+    :raises subprocess.CalledProcessError: If git encounters an exception
+    :raises SystemExit: If unmerged files are detected
+    """
     repo = bento.git.repo()
 
     if not repo:
         yield
 
     else:
+        added, removed, unmerged = git_status()
+
+        # Need to look for unmerged files first, otherwise staged_files_only will eat them
+        if unmerged:
+            echo_error(
+                "Please resolve merge conflicts in these files before continuing:"
+            )
+            for f in unmerged:
+                click.secho(f, err=True)
+            sys.exit(3)
+
         with staged_files_only(PATCH_CACHE):
             tree = cmd_output("git", "write-tree")[1].strip()
             try:
-                cmd_output("git", "reset", "--hard")
+                for a in added:
+                    Path(a).unlink()
+                cmd_output("git", "checkout", "HEAD", "--", ".")
                 yield
             finally:
-                cmd_output("git", "checkout", tree, "--", ".")
+                cmd_output("git", "checkout", tree.strip(), "--", ".")
+                if removed:
+                    cmd_output("git", "rm", *removed)
 
 
 @contextmanager
